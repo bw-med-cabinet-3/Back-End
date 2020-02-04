3c24d34b3c685acaed91e73141ea345b49e3f000
Aaron Shawn Soler <aaronshawnsoler@gmail.com>
2020-02-03 20:20:30 -0500

    cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const userRoutes = require('./routes/userRoutes');
 
 const server = express();
 server.use(express.json());
+server.use(cors());
 
 server.use('/api/users', userRoutes);
 
